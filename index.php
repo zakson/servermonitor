8c930a4ed2af807ec415da0f57085e5123e575f5
zakson <zakson@example.com>
2015-11-17 03:57:45 +0100

    poprawka hostname

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$output = 'Host Raport - '.shell_exec(hostname).PHP_EOL;
+$output = 'Host Raport - '.shell_exec('hostname').PHP_EOL;
 $output .= shell_exec('date').PHP_EOL;
 $output .= shell_exec('uptime').PHP_EOL;
 $output .= shell_exec('df -h').PHP_EOL;
